0786d1fa924b9248a5efc3c5d0ede8f2e73a599f
kennedymwavu <mwavukennedy@gmail.com>
2022-12-01 12:51:21 +0300

    part 2

--- solution.R
+++ solution.R
@@ -1,25 +1,41 @@
 inventory <- readLines('input.txt')
 
-# initialize total calories of the elf carrying the 
-# most calories to zero:
-total_calories <- 0
-
 # initialize totals for the current elf:
 current_elf <- 0
 
-for (entry in inventory) {
-    if (entry == '') {
-        if (current_elf > total_calories) {
-            total_calories <- current_elf
+# initialize top 3:
+top3 <- numeric(length = 3L)
+
+# elf counter:
+counter <- 0
+
+for (i in seq_along(inventory)) {
+    if (inventory[i] == '') {
+        counter <- counter + 1
+
+        # first 3 elves:
+        if (counter <= 3) {
+            top3[counter] <- current_elf
+            
+            # reset `current_elf`:
+            current_elf <- 0
+            next
+        }
+
+        if (current_elf > min(top3)) {
+            top3[which.min(top3)] <- current_elf
         }
 
         # reset `current_elf`:
         current_elf <- 0
-
         next
     }
 
-    current_elf <- current_elf + as.numeric(entry)
+    current_elf <- current_elf + as.numeric(inventory[i])
 }
 
-print(total_calories)
+# part 1:
+print(max(top3))
+
+# part 2:
+print(sum(top3))
